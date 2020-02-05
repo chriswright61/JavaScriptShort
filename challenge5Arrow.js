edf00c45735b8a7f5bda7b4d094424b79fd0a85e
chriswright61 <54807619+chriswright61@users.noreply.github.com>
2020-02-05 11:35:00 +0000

    Add files via upload

---
+++ challenge5Arrow.js
@@ -0,0 +1,19 @@
+// Create a function that takes two numbers and an operator as parameters.
+// It should return a print out of the sum e.g. “1 + 2 = 3” or “4 x 6 = 24”.
+
+const calcNumbers = (num1, num2, numOperator) => {
+  if (numOperator == "+") {
+    num1 + num2;
+  } else if (numOperator == "-") {
+    return num1 - num2;
+  } else if (numOperator == "*") {
+    return num1 * num2;
+  } else if (numOperator == "/") {
+    return num1 / num2;
+  }
+};
+
+console.log(calcNumbers(1, 2, "+"));
+console.log(calcNumbers(10, 2, "-"));
+console.log(calcNumbers(2, 2, "*"));
+console.log(calcNumbers(6, 3, "/"));
